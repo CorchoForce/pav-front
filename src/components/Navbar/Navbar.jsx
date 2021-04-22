773efbd14714ec39bd5c7e37c7a16519893f4ba7
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-22 15:52:03 -0300

    Adding background color

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import style from './Navbar.module.css'
 import logo from '../../images/navbar_logo.png'
+
 import {MenuItems} from '..'
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt="Web page logo" className={style.logo} width='100' height='100'/>
+    <nav className={style.navbarContainer}>
+      <div className={style.contentContainer}>
+        <img src={logo} alt="Web page logo" className={style.logo}/>
       <MenuItems/>
+      </div>
     </nav>
   );
 }
