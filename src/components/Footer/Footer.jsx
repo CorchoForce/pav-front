4775df0f7e05c96f842acc26c251c605848f82b9
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-04-22 15:12:03 -0300

    basic footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,20 @@
+import styles from './Footer.module.css'
+import Logo from '../../images/logo_white.png'
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footerContainer}>
+      <div className={styles.contentContainer}>
+        <img src={Logo} alt="White Footer Logo" className={styles.logo} />
+      </div>
+      <div className={styles.creditsContainer}>
+        <p className={styles.text}>
+          @ 2021 CorchoForce Ltda
+        </p>
+        <p>
+          Created and Developed by CorchoForce
+        </p>
+      </div>
     </footer>
   );
 }
