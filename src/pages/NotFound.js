8a9345a8df2528144fcece38af8ce8648bc0900f
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-21 23:13:24 -0300

    Updating 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,15 @@
+import logo from '../images/404_white.png';
+import '../styles/App.css';
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const NotFound = () => <h1>Not Founded</h1>
+
+const NotFound = () => 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">Page not founded</Link>
+      </header>
+    </div>
 
 export default NotFound
