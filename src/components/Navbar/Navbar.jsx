756d30f2bfc021b2ff078a617c089c687eb76696
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-22 14:59:08 -0300

    Adding MenuItems on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import style from './Navbar.module.css'
 import logo from '../../images/navbar_logo.png'
+import {MenuItems} from '..'
+
 const Navbar = () => {
   return (
     <nav>
       <img src={logo} alt="Web page logo" className={style.logo} width='100' height='100'/>
+      <MenuItems/>
     </nav>
   );
 }
