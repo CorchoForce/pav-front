c736f17b320ede4725bde660730832a03b950193
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-22 14:18:42 -0300

    Adding logo to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import style from './Navbar.module.css'
+import logo from '../../images/navbar_logo.png'
 const Navbar = () => {
   return (
     <nav>
+      <img src={logo} alt="Web page logo" className={style.logo} width='300' height='300'/>
     </nav>
   );
 }
