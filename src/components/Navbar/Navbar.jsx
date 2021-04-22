4b2b868bc741bf83c13adfc4aa6cce71047cba29
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-22 14:22:22 -0300

    Updating logo size on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../../images/navbar_logo.png'
 const Navbar = () => {
   return (
     <nav>
-      <img src={logo} alt="Web page logo" className={style.logo} width='300' height='300'/>
+      <img src={logo} alt="Web page logo" className={style.logo} width='100' height='100'/>
     </nav>
   );
 }
