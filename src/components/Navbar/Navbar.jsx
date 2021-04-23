2b48b8e258299b772656e59fbd0ec2f7bc80f15d
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-23 16:58:16 -0300

    Removing unnecessary div from navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,9 +7,7 @@ const Navbar = () => {
   return (
     <nav className={style.navbar}>
       <img src={logo} alt="Web page logo" className={style.navbarLogo} />
-      <div className={style.contentContainer}>
-        <MenuItems />
-      </div>
+      <MenuItems />
     </nav>
   );
 };
