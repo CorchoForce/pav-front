c2fa8a62ed35586b40a9f797ae379db3495d171e
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-23 13:14:42 -0300

    Adding underlined on navbar text

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ import style from './MenuItem.module.css'
 const MenuItem = ({link, text}) => {
   return (
     <li className={style.item}>
-      <Link to={link}>
+      <Link to={link} className={style.link}>
         <span className={style.text}>{text}</span>
       </Link>
     </li>
