26fff81f403a3456036fca82f0747cf0ec86b84a
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-04-23 15:00:29 -0300

    Updating images to svg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import style from './Navbar.module.css'
-import logo from '../../images/navbar_logo.png'
+import style from "./Navbar.module.css";
+import logo from "../../images/navbar_logo.svg";
 
-import {MenuItems} from '..'
+import { MenuItems } from "..";
 
 const Navbar = () => {
   return (
     <nav className={style.navbar}>
-      <img src={logo} alt="Web page logo" className={style.navbarLogo}/>
+      <img src={logo} alt="Web page logo" className={style.navbarLogo} />
       <div className={style.contentContainer}>
-        <MenuItems/>
+        <MenuItems />
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
