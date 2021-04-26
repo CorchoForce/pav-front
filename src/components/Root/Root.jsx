24d29bac08dc8b17d3633e6d80b61567667230ff
Eduardo <eduardogds@poli.ufrj.br>
2021-04-26 19:32:34 -0300

    Removing unnecessary components

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,6 +1,19 @@
 import React from "react";
-import { Routes } from "..";
-
-const Root = () => <Routes />;
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { About, Home, NotFound, Login, Register } from "../../pages";
+import { Layout } from "..";
+const Root = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route sensitive exact path="/" component={Home} />
+        <Route sensitive exact path="/about" component={About} />
+        <Route sensitive exact path="/register" component={Register} />
+        <Route sensitive exact path="/login" component={Login} />
+        <Route sensitive exact path="*" component={NotFound} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default Root;
