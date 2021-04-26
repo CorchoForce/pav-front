1efbaa10ed50f13556dced67f38e6bf3b87a722c
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-04-26 19:35:13 -0300

    finish footer + fix navbar on other browsers

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,18 @@
 import styles from './Footer.module.css'
-import Logo from '../../images/logo_white.png'
 
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-      <div className={styles.contentContainer}>
-        <img src={Logo} alt="White Footer Logo" className={styles.logo} />
-      </div>
-      <div className={styles.creditsContainer}>
-        <p className={styles.text}>
-          @ 2021 CorchoForce Ltda
-        </p>
-        <p className={styles.text}>
-          Created and Developed by CorchoForce
-        </p>
-      </div>
+      <p>
+        @ 2021 CorchoForce Ltda
+      </p>
+      <p>
+        Created and Developed by CorchoForce
+      </p>
+      <p>
+        Quer ofertar uma vaga ? Entre em contato em 
+        <span className={styles.email}>{" corchoforce@gmail.com"}</span>
+      </p>
     </footer>
   );
 }
