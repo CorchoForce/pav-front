16a82d3548618b389d2545b7cfcaa18149b19d3a
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-05-31 19:47:55 -0300

    Adding SignUp page

--- src/components/MenuItems/MenuItems.jsx
+++ src/components/MenuItems/MenuItems.jsx
@@ -2,10 +2,13 @@ import { MenuItem } from "..";
 import styles from "./MenuItems.module.css";
 
 const MenuItems = () => {
+  const loggedIn = false
+
   return (
     <ul className={styles.menu}>
       <MenuItem link="/" text="Home" />
       <MenuItem link="/about" text="Sobre" />
+      {loggedIn ? <MenuItem link="/profile" text="Meu Perfil" /> : <MenuItem link="/sign_in" text="Login" /> }
     </ul>
   );
 };
--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -9,17 +9,15 @@ const SignInForm = () => {
     console.log("fazer login com senha = " + password + " e email = " + email)
     event.preventDefault()
   }
-  const handleEmail = (event) => {
-    setEmail(event.target.value)
-  }
-  const handlePassword = (event) => {
-    setPassword(event.target.value)
+
+  const inputHandler = (event, setter) => {
+    setter(event.target.value)
   }
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <input className={styles.formInput} type="mail" onChange={handleEmail} placeholder="Email" required />
-      <input className={styles.formInput} type="password" onChange={handlePassword} placeholder="Senha" required />
+      <input className={styles.formInput} type="mail" onChange={(e) => inputHandler(e, setEmail)} placeholder="Email" required />
+      <input className={styles.formInput} type="password" onChange={(e) => inputHandler(e, setPassword)} placeholder="Senha" required />
       <button className={styles.formButton} type="submit">
         Entrar!
       </button>
