4099a320c746395c3e6dffd9cab88a0cb7b78994
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-02 00:09:40 -0300

    Changing  register offer useState

--- src/components/RegisterOfferForm/RegisterOfferForm.jsx
+++ src/components/RegisterOfferForm/RegisterOfferForm.jsx
@@ -3,56 +3,40 @@ import { useState } from "react";
 
 const RegisterOfferForm = () => {
 
-  const [email, setEmail] = useState(undefined);
-  const [title, setTitle] = useState(undefined);
-  const [type, setType] = useState(undefined);
-  const [tags, setTags] = useState(undefined);
-  const [requirements, setRequirements] = useState(undefined);
-  const [websiteUrl, setWebsiteUrl] = useState(undefined);
-  const [description, setDescription] = useState(undefined);
-  const [limitDate, setLimitDate] = useState(undefined);
-  const [salary, setSalary] = useState(undefined);
-  const [startDate, setStartDate] = useState(undefined);
-  const [location, setLocation] = useState(undefined);
-  const [neededHours, setNeededHours] = useState(undefined);
-
-
-    const inputHandler = (event, setter) => {
-      setter(event.target.value);
-    };
+  const [offer, setOffer] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log(
-      "Cadastrar uma oferta com: Título= " + title + "\nEmail= " + email +
-      "\nTipo= " + type + "\nTags= " + tags + "\nRequisitos= " + requirements +
-      "\nWebsiteUrl= " + websiteUrl + "\nDescrição= " + description +
-      "\nDataLimite= " + limitDate + "\nSalário= " + salary +
-      "\nDataInicio= " + startDate + "\nLocalização= " + location +
-      "\nHorasNecessarias= " + neededHours
+      "Oferta Cadastrada Com sucesso: \nTítulo= " + offer.title + "\nEmail= " + offer.email +
+      "\nTipo= " + offer.type + "\nTags= " + offer.tags + "\nRequisitos= " + offer.requirements +
+      "\nWebsiteUrl= " + offer.websiteUrl + "\nDescrição= " + offer.description +
+      "\nDataLimite= " + offer.limitDate + "\nSalário= " + offer.salary +
+      "\nDataInicio= " + offer.startDate + "\nLocalização= " + offer.location +
+      "\nHorasNecessarias= " + offer.neededHours
     );
   }
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setTitle)} placeholder="Título" required />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setType)} placeholder="Tipo" required />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setRequirements)} placeholder="Requisitos" />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setWebsiteUrl)} placeholder="Site" />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setTags)} placeholder="Tags" required />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setDescription)} placeholder="Descrição" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, title: e.target.value})} placeholder="Título" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, type: e.target.value})} placeholder="Tipo" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, requirements: e.target.value})} placeholder="Requisitos" />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, websiteUrl: e.target.value})} placeholder="Site" />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, tags: e.target.value})} placeholder="Tags" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, description: e.target.value})} placeholder="Descrição" required />
       <div className = {styles.dateWrapper}>
         <label>Data limite para aplicação</label>
-        <input className={styles.formInput} onChange={(e) => inputHandler(e, setLimitDate)} type="date" required />
+        <input className={styles.formInput} onChange={(e) => setOffer({...offer, limitDate: e.target.value})} type="date" required />
       </div>
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setSalary)} placeholder="Salário"required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, salary: e.target.value})} placeholder="Salário"required />
       <div className = {styles.dateWrapper}>
         <label>Data de Ingresso</label>
-        <input className={styles.formInput} onChange={(e) => inputHandler(e, setStartDate)} type="date" required />
+        <input className={styles.formInput} onChange={(e) => setOffer({...offer, startDate: e.target.value})} type="date" required />
       </div>
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setLocation)}placeholder="Localização" required />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setNeededHours)} placeholder="Horas necessárias" required />
-      <input className={styles.formInput} type="text" onChange={(e) => inputHandler(e, setEmail)} placeholder="Email para contato" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, location: e.target.value})}placeholder="Localização" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, nededHours: e.target.value})} placeholder="Horas necessárias" required />
+      <input className={styles.formInput} type="text" onChange={(e) => setOffer({...offer, email: e.target.value})} placeholder="Email para contato" required />
       <button className={styles.formButton} type="submit"> Enviar </button>
     </form>
   );
