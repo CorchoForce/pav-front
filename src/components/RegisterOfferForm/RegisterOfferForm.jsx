31e1c655c09caa077dba1c85a2309cf1427f167c
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-01 23:13:05 -0300

    Adding RegisterOffer Component

--- src/components/RegisterOfferForm/RegisterOfferForm.jsx
+++ src/components/RegisterOfferForm/RegisterOfferForm.jsx
@@ -1,7 +1,83 @@
 import styles from "./RegisterOfferForm.module.css";
 
 const RegisterOfferForm = () => {
-  return <form className={styles.form}></form>;
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Título"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Tipo"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Requisitos"
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        onChange={(e) => inputHandler(e, setEmail)}
+        placeholder="Site"
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Tags"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Descrição"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="date"
+        placeholder="Data Limite para aplicação"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Salário"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="date"
+        placeholder="Data de Início"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        onChange={(e) => inputHandler(e, setEmail)}
+        placeholder="Localização"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        onChange={(e) => inputHandler(e, setEmail)}
+        placeholder="Horas necessárias"
+        required
+      />
+      <input
+        className={styles.formInput}
+        type="text"
+        placeholder="Email para contato"
+        required
+      />
+    </form>
+  );
 };
 
 export default RegisterOfferForm;
