945ff9f606b637645a44d8803d57220dd1620f7c
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-06-05 19:34:14 -0300

    minor changes

--- src/components/MyCards/MyCards.jsx
+++ src/components/MyCards/MyCards.jsx
@@ -46,8 +46,8 @@ const MyCards = () => {
         return (
           <Card data={data} key={data._id}>
             <div style={{display: "flex", justifyContent: 'space-between', alignItems: 'center', width: '22%' }}>
-              {data.valid ? <img src={validated} alt='validated' className={styles.trash} /> : <img src={toBeValidated} alt='to be validated' className={styles.trash} />}
-              <img src={trash} alt='delete' className={styles.trash} onClick={(e) => {
+              {data.valid ? <img src={validated} alt='validated' className={styles.icon} /> : <img src={toBeValidated} alt='to be validated' className={styles.icon} />}
+              <img src={trash} alt='delete' className={styles.icon} onClick={(e) => {
                 deleteCard(data._id);
                 e.preventDefault()
               }}/>
