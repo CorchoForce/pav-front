9ce7814f11a32267f7fd86c5a1016150bdf1a01c
Tales Moreira <talesmoreira@poli.ufrj.br>
2021-06-08 02:58:46 -0300

    About page partial remake
    About page changed design. Not final.

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,10 @@ const About = () => {
             <BrowserView>
             <div className={styles.aboutContainer}>
                 <div className={styles.contentContainer}>
-                    <h1 className={styles.title}>Sobre</h1>
+                    <h1 className={styles.title}>Sobre <br></br>Pega a visão</h1>
+                </div>
+                <div className={styles.line}></div>
+                <div className={styles.gridContainer}>
                     <div className={styles.aboutContent}>
                         <p>Quantas vezes, na UFRJ, o aluno teve problemas para encontrar a sua tão sonhada vaga de Iniciação científica, Extensão, Equipes de competição ou até mesmo estágio, no qual este ficou a mercê de emails dos coordenadores ou até mesmo dos anunciantes da vaga?</p>
                         <p>Pois bem, nós da CorchoForce, um grupo formado por 6 integrantes, teve a ideia de solucionar este problemas e lhe apresentamos a pega a visão.</p>
@@ -19,14 +22,6 @@ const About = () => {
                         <p>Pega a visão é um projeto desenvolvido que consiste em organizar todas as ofertas (estágio, Iniciação científica, extensão, etc) e dispor aos alunos, de modo que estes possam aplicar pela própria plataforma <i>(eventualmente)</i>, além de receber notificações pelo seu e-mail quando aparecer uma nova oportunidade.</p>
                         <p>A ideia se baseou em conversas que tivemos com 10 pessoas que já fizeram iniciação científica e ambos apresentaram descontentamentos na forma de divulgação das vagas e decidimos ampliar para todas as vagas possíveis. </p>
                     </div>
-                </div>
-                <div className={styles.line}></div>
-                <div className={styles.gridContainer}>
-                    <div className={styles.gridElement1}> </div>
-                    <div className={styles.gridElement2}> </div>
-                    <div className={styles.gridElement3}> </div>
-                    <div className={styles.gridElement4}> </div>
-                    <div className={styles.gridElement5}> </div>
                 </div>
             </div>
             </BrowserView>
