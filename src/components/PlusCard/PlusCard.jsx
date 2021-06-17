5f3b86fb3719b03aef8e9b6d39a841bc31f7e196
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-16 21:09:19 -0300

    Adding new offer message

--- src/components/PlusCard/PlusCard.jsx
+++ src/components/PlusCard/PlusCard.jsx
@@ -4,14 +4,15 @@ import plus from '../../images/plus.svg'
 
 const PlusCard = () => {
   return (
-    <Link to='/register_offer' className={styles.link}>
+    <Link to="/register_offer" className={styles.link}>
       <div className={styles.card}>
+        <p className={styles.text}>Clique aqui para adicionar uma nova oferta</p>
         <div className={styles.plus}>
-          <img src={plus} alt="Plus sign" className={styles.plusImage}/>
+          <img src={plus} alt="Plus sign" className={styles.plusImage} />
         </div>
       </div>
     </Link>
-  )  
+  );  
 }
 
 export default PlusCard
